34b4c954b3661875d1e5cc4f00e19f1565a157ac
jcdelgado <jcdelgado@jcdelgado.(none)>
2013-08-30 12:59:08 +0200

    finally

--- app/views.py
+++ app/views.py
@@ -12,18 +12,26 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         db = MongoClient().egarbage
         measures = db.measures
-        cursor = measures.find()
+        cursor1 = measures.find({"id_device": 4098})
+        cursor2 = measures.find({"id_device": 4101})
         measure_list = []
-        for n in range(cursor.count()):
-            next = cursor.next()
-
-            chartdata = [float(next['date'].strftime("%s")+"000"), next['measure']]
+        measure_list2 = []
+        for n in range(cursor1.count()):
+            next = cursor1.next()
+            chartdata = [float(next['date'].strftime("%s")), next['measure']]
             measure_list.append(chartdata)
+        print "Match %s results" % len(measure_list)
+
+        for n in range(cursor2.count()):
+            next = cursor2.next()
+            chartdata = [float(next['date'].strftime("%s")), next['measure']]
+            measure_list2.append(chartdata)
         print "Match %s results" % len(measure_list)
 
 
         context = super(HomePageView, self).get_context_data(**kwargs)
         context['chartdata'] = measure_list
+        context['chartdata2'] = measure_list2
         #context['latest_articles'] = Article.objects.all()[:5]
         return context
 
