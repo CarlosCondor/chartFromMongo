483368ea0b5d9fd82c2c0e7c9d8eef218704b239
jcdelgado <jcdelgado@jcdelgado.(none)>
2013-09-05 14:38:09 +0200

    Clean && refactor

--- app/views.py
+++ app/views.py
@@ -12,8 +12,6 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         db = MongoClient().egarbage
         measures = db.measures
-        cursor1 = measures.find({"id_device": 4098})
-        cursor2 = measures.find({"id_device": 4101})
         distintos = measures.distinct("id_device")
         cursores = []
         chartdatas = []
@@ -29,24 +27,10 @@ class HomePageView(TemplateView):
             chartdatas.append(tempdatas)
 
         print "Distintos: ", distintos
-        measure_list = []
-        measure_list2 = []
-        for n in range(cursor1.count()):
-            next = cursor1.next()
-            chartdata = [float(next['date'].strftime("%s")), next['measure']]
-            measure_list.append(chartdata)
-        print "Match %s results" % len(measure_list)
-
-        for n in range(cursor2.count()):
-            next = cursor2.next()
-            chartdata = [float(next['date'].strftime("%s")), next['measure']]
-            measure_list2.append(chartdata)
-        print "Match %s results" % len(measure_list2)
-
+        
 
         context = super(HomePageView, self).get_context_data(**kwargs)
-        context['chartdata'] = measure_list
-        context['chartdata2'] = measure_list2
+    
         context['chartdatas'] = chartdatas
         #context['latest_articles'] = Article.objects.all()[:5]
         return context
