9aef02b2551d141f481dc8009fcaad96aa771571
jcdelgado <jcdelgado@jcdelgado.(none)>
2013-09-05 16:39:34 +0200

    Added alert for synchronization errors

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@
 from pymongo import MongoClient
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
-
+import datetime
 
 class HomePageView(TemplateView):
 
@@ -15,24 +15,31 @@ class HomePageView(TemplateView):
         distintos = measures.distinct("id_device")
         cursores = []
         chartdatas = []
+        syncerrors = []
         for ide in distintos:
             cursores.append(measures.find({"id_device": ide}))
-
+        ccc=0
         for c in cursores:
             tempdatas = []
             for n in range(c.count()):
                 next = c.next()
                 chartdata = [float(next['date'].strftime("%s")), next['measure']]
                 tempdatas.append(chartdata)
+            if (float(datetime.datetime.now().strftime("%s"))-tempdatas[-1][0]) > 10*60:
+                syncerrors.append("%s no sincroniza desde %s" % (distintos[ccc], datetime.datetime.fromtimestamp(float(tempdatas[-1][0]))))
+            print tempdatas[-1]
             chartdatas.append(tempdatas)
+            ccc+=1
 
         print "Distintos: ", distintos
+        print syncerrors
         
 
         context = super(HomePageView, self).get_context_data(**kwargs)
     
         context['chartdatas'] = chartdatas
         context['chartlabels'] = distintos
+        context['syncerrors'] = syncerrors
         #context['latest_articles'] = Article.objects.all()[:5]
         return context
 
